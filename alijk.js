4842e33a5e997f79b914a91c626e6854280f2ef6
pengwenqiang <416580612@qq.com>
2015-04-20 15:56:02 +0800

    dev branch

--- alijk.js
+++ alijk.js
@@ -3,7 +3,7 @@
 window.doseusages:药品用法,{id:{id,name}}
 window.dosefrequencies:药品频次,{id:{id,name}}
 */
-//test test1
+//test te1s1t1
 ;(function(w){
     var rvalidchars = /^[\],:{}\s]*$/,
         rvalidbraces = /(?:^|:|,)(?:\s*\[)+/g,
